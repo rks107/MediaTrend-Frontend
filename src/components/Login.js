7aade21f59ee08e1b970b3b55d6d3b324f5e393e
ROHIT KUMAR <sumitkumar@rohit-2.local>
2020-12-14 00:30:45 +0530

    Form Handling: Controlled Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    render() {
-        return (
-          <form className="login-form">
-            <span className="login-signup-header">Log In</span>
-            <div className="field">
-              <input type="email" placeholder="Email" required />
-            </div>
-            <div className="field">
-              <input type="password" placeholder="Password" required />
-            </div>
-            <div className="field">
-                <button>Log In</button>
-            </div>
-          </form>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleFormSubmite = (e) => {
+      e.preventDefault();
+      console.log(this.state);
+  };
+  render() {
+    return (
+      <form className="login-form">
+        <span className="login-signup-header">Log In</span>
+        <div className="field">
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
+        </div>
+        <div className="field">
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
+        </div>
+        <div className="field">
+          <button onClick={this.handleFormSubmite}>Log In</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Login;
